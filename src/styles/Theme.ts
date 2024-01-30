9a0ce5b4b20cec786b999aa746af334604696bd4
kurt2019 <artemkurt@ukr.net>
2024-01-30 16:32:39 +0200

    Config GlobalStyle in index.tsx and theme

---
+++ src/styles/Theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+    colors: {
+        primaryBg: "#041F31",
+        mainFont: "#BDEBEA",
+        secondaryFont: "#252728"
+    },
+
+    media: {
+        tablet: "screen and (max-width: 768px)",
+        mobile: "screen and (max-width: 576px)",
+        desktop: "screen and (min-width: 1140px)",
+    }
+}
